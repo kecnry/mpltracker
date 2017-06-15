fbaecb35782f8371ab9aff4e847cf6fb35e821f6
Kyle Conroy <kyleconroy@gmail.com>
2017-06-15 18:45:25 -0400

    fixed bug when running from installed module
    this closes #1

--- mpltracker/modulehacker.py
+++ mpltracker/modulehacker.py
@@ -19,14 +19,21 @@ class Hacker:
 class Loader:
     def __init__(self):
         self.module = None
-    
+
     def find_module(self, name, path):
+        # print "*** modulehacker.find_module", name, path
         sys.meta_path.remove(self)
+        # not entirely sure why this is necessary, but the parent mpltracker
+        # module is import functions with the mpltracker. name which causes
+        # everything to fail when installed.  This fixes https://github.com/kecnry/mpltracker/issues/1
+        if 'mpltracker.' in name:
+            name = '.'.join(name.split('.')[1:])
         self.module = importlib.import_module(name)
         sys.meta_path.insert(0, self)
         return self
-    
+
     def load_module(self, name):
+        # print "*** modulehacker.load_module", name
         if not self.module:
             raise ImportError("Unable to load module.")
         module = self.module
