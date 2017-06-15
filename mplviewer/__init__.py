a66ee391d6bfedfe8613b030a7e232d958192f30
Kyle Conroy <kyleconroy@gmail.com>
2017-06-15 13:55:20 -0400

    moved files and created setup script

---
+++ mplviewer/__init__.py
@@ -0,0 +1,1 @@
+from mplviewer import *
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mplviewer',
+      version='0.1.0',
+      description='Matplotlib tracker',
+      author='Kyle Conroy',
+      author_email='kyleconroy@gmail.com',
+      url='https://www.github.com/kecnry/mpl-viewer',
+      packages=['mplviewer'],
+     )
